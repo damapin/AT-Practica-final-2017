476df05c3744df05bd260f55654e1f84279869fd
David Marín Del Pino <damapin@gmail.com>
2017-05-09 11:38:03 +0200

    Cargados aparcamientos desde JSON y añadida lista de los mismos con scroll

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+var facilities;
+
+function showFacilities(){
+  var l = facilities["@graph"].length;
+  var list = document.getElementById('facilities-list');
+  list.innerHTML = "";
+    for (var i = 0; i<l; i++ ) {
+      //console.log(facilities["@graph"][i].id + " : " + facilities["@graph"][i].title);
+      var facility = document.createElement("a");
+      facility.appendChild(document.createTextNode(facilities["@graph"][i].title));
+      list.appendChild(facility);
+      list.appendChild(document.createElement("br"));
+    }
+}
+
+function loadFacilities(url){
+  $.ajax({
+    dataType: "json",
+    url: url,
+    success: function(data){
+      facilities = data;
+    }
+  }).done(function(){
+    showFacilities();
+  });
+}
+
 $(document).ready(function() {
 
   $("#main-tab").show(600,"linear");
@@ -29,4 +56,7 @@ $(document).ready(function() {
     $("#main-tab").hide(500,"linear");
   });
 
+  $('#load_facilities').click(function(){
+    loadFacilities("resources/facilities.json");
+  });
 });
