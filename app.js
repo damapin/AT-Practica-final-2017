36af09d686c93b2a3f0ec0391c2b9155ecf39ebe
David Marín Del Pino <damapin@gmail.com>
2017-05-09 17:02:09 +0200

    Añadida funcionalidad para seleccionar instalaciones concretas y mostrar su información

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
 var facilities;
 
-function showFacilities(){
-  var l = facilities.length;
-  var list = document.getElementById('facilities-list');
-  list.innerHTML = "";
-    for (var i = 0; i<l; i++ ) {
-      var facility = facilities[i];
-      var facilityType = facility.title.split(".",1)[0];
-      var facilityAddr = facility.address;
-      var listItem = document.createElement("a");
-      listItem.appendChild(document.createTextNode(facilityType + ", " + facilityAddr["street-address"]));
-      list.appendChild(listItem);
-      list.appendChild(document.createElement("br"));
-    }
-}
-
 function loadFacilities(url){
   $.ajax({
     dataType: "json",
@@ -27,11 +12,47 @@ function loadFacilities(url){
   });
 }
 
+function showFacilities(){
+  var l = facilities.length;
+  var facilitiesListLocation = document.getElementById('facilities-list');
+  var list = document.createElement("ul");
+  facilitiesListLocation.innerHTML = "";
+    for (var i = 0; i<l; i++ ) {
+      var facility = facilities[i];
+      var facilityID = facility.id;
+      var facilityType = facility.title.split(".",1)[0];
+      var facilityAddr = facility.address;
+      var listItem = document.createElement("li");
+      listItem.innerHTML = "<a onclick='chooseFacility(" + facilityID + ")'>" + facilityType + ", " + facilityAddr["street-address"] + "</a>";
+      list.appendChild(listItem);
+    }
+  facilitiesListLocation.appendChild(list);
+}
+
 function initMap(){
   mymap = L.map('facilities-map').setView([40.416826, -3.703535], 16);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mymap);
+}
+
+function chooseFacility(id){
+  var choosenFacility;
+  var l = facilities.length;
+  for (var i = 0; i < l; i++) {
+    if (facilities[i].id == id || facilities[l-i] == id) {
+      choosenFacility = facilities[i];
+      facilityInfo = document.getElementById('facility-info');
+      facilityInfo.innerHTML = "";
+      console.log("Aparcamiento con id: " + id + " localizado");
+      var title = document.createElement("p");
+      title.appendChild(document.createTextNode(choosenFacility.title));
+      facilityInfo.appendChild(title);
+      var address = document.createElement("p");
+      address.appendChild(document.createTextNode("Dirección: " + choosenFacility.address["street-address"]));
+      facilityInfo.appendChild(address);
+    }
+  }
 }
 
 $(document).ready(function() {
